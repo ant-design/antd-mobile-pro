508315a61c0150d7c33fdac2bc2de573694e8c67
xiaohuoni <chen.xiaocong2@iwhalecloud.com>
2022-07-19 15:50:07 +0800

    chore: lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: require.resolve('alita/eslint')
-}
+  extends: require.resolve('alita/eslint'),
+  rules: {
+    'selector-class-pattern': 0,
+  },
+};
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: require.resolve('alita/stylelint')
-}
+  extends: require.resolve('alita/stylelint'),
+};
